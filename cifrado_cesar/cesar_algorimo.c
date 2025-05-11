04adba377c0ce2c02edf2e6f1c1c3b2199c929fe
Antares0922 <juanm042209@gmail.com>
2025-05-11 17:58:50 -0600

    cesar completado

--- cifrado_cesar/cesar_algorimo.c
+++ cifrado_cesar/cesar_algorimo.c
@@ -13,7 +13,7 @@ int main()
     palabra[strcspn(palabra,"\n")] = '\0';
     //longitud de la cadena
     int longitud = (int)strlen(palabra);
-    printf("%i\n",longitud);
+    printf("la longitud de tu palabra es de %i\n",longitud);
     algoritmo(palabra,longitud);
     return 0;
 }
@@ -21,22 +21,26 @@ int main()
 void algoritmo(char palabra[100],int longitud)
 {
     char codificada[100];
-    codificada[0] = '\0';
     char abc[29] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c'};
     //recorre cada letra de tu palabra
     for (int indice = 0; indice<longitud;indice++)
     {
         char letra = palabra[indice];
         //recorre el abc
-        for(int i = 0; i<27;i++)
+        for(int i = 0; i<=26;i++)
         {
             if (letra == abc[i])
             {
                 codificada[indice] = abc[i+3];
                 break;
             }
+            else
+            {
+                codificada[indice] = ' ';
+            }
         }
     }
-    //imprime entre 1 a 3 caracteres basura dependiendo de la longitud
-    printf("%s",codificada);
+    //añadir \0
+    codificada[longitud] = '\0';
+    printf("la palabra es %s",codificada);
 }
