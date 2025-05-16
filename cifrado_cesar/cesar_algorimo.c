731fbdbf18da4fa74cdac746110e6eedfb53dc82
Antares0922 <juanm042209@gmail.com>
2025-05-15 22:03:19 -0600

    uso de memoria dinamica en str

--- cifrado_cesar/cesar_algorimo.c
+++ cifrado_cesar/cesar_algorimo.c
@@ -1,53 +1,83 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void algoritmo(char palabra[100],int longitud,char *puntero);
+void cifrado(int longitud,char *pPalabra);
+void descifrado(int longitud,char *pPalabra);
 
 int main()
 {
-    char palabra[100];
-    printf("escribe una palabra:");
-    //obtiene todo del teclado
-    fgets(palabra,sizeof(palabra),stdin);
-    //quitando el enter
-    palabra[strcspn(palabra,"\n")] = '\0';
-    //longitud de la cadena
-    int longitud = (int)strlen(palabra);
-    printf("la longitud de tu palabra es de %i\n",longitud);
-    //puntero a palabra
-    char *pPalabra = palabra;
-    algoritmo(palabra,longitud,pPalabra);
-    printf("tu palabra es %s\n",palabra);
+    //array
+    char *pPalabra;
+    int longitud = 0;
+    char temp;
+    //rellenando array
+    printf("Escribe una oracion:\n");
+
+    //necesita leer que ya termino con ctrl + z
+    while ((temp = getchar()) != '\n')
+    {
+        // Aumentar el tamaño en 1 por cada caracter
+        pPalabra = realloc(pPalabra, longitud + 1); 
+        pPalabra[longitud] = temp;
+        longitud++;
+    }
+    //aunmentando el tamaño para el caracter final '\0'
+    pPalabra = realloc(pPalabra, longitud + 1);
+    pPalabra[longitud] = '\0';
+    //cifrado y desifrado
+    printf("tu palabra original es: %s\n",pPalabra);
+    cifrado(longitud,pPalabra);
+    printf("tu palabra cifrada es: %s\n",pPalabra);
+    descifrado(longitud,pPalabra);
+    printf("tu palabra descifrada es: %s\n",pPalabra);
+    //liberando memoria
+    free(pPalabra);
     return 0;
 }
 
-void algoritmo(char palabra[100],int longitud,char *puntero)
+void cifrado(int longitud,char *pPalabra)
 {
-    char codificada[100];
     char abc[29] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c'};
     //recorre cada letra de tu palabra
     for (int indice = 0; indice<longitud;indice++)
     {
-        char letra = palabra[indice];
+        char letra = pPalabra[indice];
         //recorre el abc
         for(int i = 0; i<=26;i++)
         {
             if (letra == abc[i])
             {
-                codificada[indice] = abc[i+3];
+                pPalabra[indice] = abc[i+3];
                 break;
             }
             else
             {
-                codificada[indice] = ' ';
+                pPalabra[indice] = ' ';
             }
         }
     }
-    //añadir \0
-    codificada[longitud] = '\0';
-    //modificando palabra
-    for (int x = 0; x<longitud+1;x++)
+}
+
+void descifrado(int longitud,char *pPalabra)
+{
+    char abc[29] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c'};
+    //recorre cada letra de tu palabra
+    for (int indice = 0; indice<longitud;indice++)
     {
-        *(puntero + x) = codificada[x];
+        char letra = pPalabra[indice];
+        //recorre el abc
+        for(int i = 0; i<=26;i++)
+        {
+            if (letra == abc[i])
+            {
+                pPalabra[indice] = abc[i-3];
+                break;
+            }
+            else
+            {
+                pPalabra[indice] = ' ';
+            }
+        }
     }
 }
